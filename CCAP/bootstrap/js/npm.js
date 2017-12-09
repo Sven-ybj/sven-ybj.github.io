97f64129305a23bb725093c453db5b8f64335206
Sven <ybjunasp@163.com>
2017-12-09 23:47:59 +0800

    1
    1

---
+++ CCAP/bootstrap/js/npm.js
@@ -0,0 +1,3 @@
+/*! my-project-name 0.1.0 2017-12-09 */
+
+require("../../js/transition.js"),require("../../js/alert.js"),require("../../js/button.js"),require("../../js/carousel.js"),require("../../js/collapse.js"),require("../../js/dropdown.js"),require("../../js/modal.js"),require("../../js/tooltip.js"),require("../../js/popover.js"),require("../../js/scrollspy.js"),require("../../js/tab.js"),require("../../js/affix.js");
